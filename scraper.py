e0439238229738ab3e4311008ea8f8a2c18ab6cd
Hackons la corruption <pascal.robichaud.do101@gmail.com>
2015-07-13 21:03:34 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -10,4 +10,8 @@ dom = PyQuery('%s/fr/projets' % ROOT)
 
 #print q('.ratesdetails').text()
 
+#Fin du traitement
+traitement_fin = datetime.now() 
+print("Fin du traitement: %s " % traitement_fin)
+
 
