8f1f67b0f1ef45ca815ef21a771e8c07954abeaf
Hackons la corruption <pascal.robichaud.do101@gmail.com>
2015-07-13 21:04:34 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,6 +4,12 @@ from pyquery import PyQuery
 ROOT = 'https://fairemtl.ca'
 dom = PyQuery('%s/fr/projets' % ROOT)
 
+pages = int(dom.find('.pager-last a').attr('href').split('%2C')[-1])
+
+urls = []
+for page in range(0, pages + 1):
+    dom = pyquery.PyQuery('%s/fr/projets?page=0,%s' % (ROOT, page))
+    urls.extend("%s%s" % (ROOT, a.attrib['href']) for a in dom.find('h3 a'))
 #url = 'http://www.vrbo.com/373926'
 
 #q = PyQuery(url)
@@ -11,7 +17,6 @@ dom = PyQuery('%s/fr/projets' % ROOT)
 #print q('.ratesdetails').text()
 
 #Fin du traitement
-traitement_fin = datetime.now() 
-print("Fin du traitement: %s " % traitement_fin)
+print("Fin du traitement")
 
 
